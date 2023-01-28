130bc43b91b5fc975ba70795d8f302978ff85dff
rohitbansal11 <rbansal000888@gmail.com>
2023-01-28 16:09:13 +0530

    nwfilrufn

--- utils/axios.js
+++ utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "http://13.232.161.74:5002/",
+  baseURL: "http://43.204.235.164:5002/",
   //baseURL: "https://gud-guru-api-rohitbansal11.vercel.app/api/v1",
   // baseURL: "http://localhost:5000/api/v1",
 });
